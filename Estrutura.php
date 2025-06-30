49b58ab87bdbbe9b980377d8174e4aa690a4f7df
Henrique Dias <128394455+HenriqueDias20@users.noreply.github.com>
2025-06-29 22:37:14 -0300

    Create Estrutura.php

---
+++ Estrutura.php
@@ -0,0 +1,9 @@
+trabalho-final-ia/
+├── modelo/
+│   └── modelo_digitos.h5         
+├── imagens_teste/
+│   └── exemplo_1.png             
+├── treino_modelo.py             
+├── reconhecimento.py            
+├── README.md                    
+└── relatorio.pdf                 
