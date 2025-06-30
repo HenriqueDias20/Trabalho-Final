20013df5b6888973804c12dcaaa58dc034076df4
Henrique Dias <128394455+HenriqueDias20@users.noreply.github.com>
2025-06-29 22:40:27 -0300

    reconhecimento.py

--- reconhecimento.py
+++ reconhecimento.py
@@ -4,19 +4,16 @@ import numpy as np
 from PIL import Image
 import sys
 
-# Carrega modelo
 model = load_model('modelo/modelo_digitos.h5')
 
-# Carrega imagem
 def carregar_imagem(caminho):
-    img = Image.open(caminho).convert('L')  # preto e branco
+    img = Image.open(caminho).convert('L') 
     img = img.resize((28, 28))
     img_array = np.array(img)
-    img_array = 255 - img_array  # inverter preto e branco
+    img_array = 255 - img_array 
     img_array = img_array / 255.0
     return img_array.reshape(1, 28, 28)
 
-# Previsão
 if len(sys.argv) < 2:
     print("Uso: python reconhecimento.py imagens_teste/exemplo_1.png")
     sys.exit()
